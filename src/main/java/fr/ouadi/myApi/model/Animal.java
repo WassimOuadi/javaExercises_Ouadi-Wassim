313f5e2f597118c69fd0e51a532d194470cc9592
OuadiWassim <wassimouadi2@gmail.com>
2023-06-26 10:52:42 +0200

    creation classe animal

---
+++ src/main/java/fr/ouadi/myApi/model/Animal.java
@@ -0,0 +1,32 @@
+package fr.ouadi.myApi.model;
+
+public class Animal {
+    private int id;
+    private String name;
+    private String type;
+    private int weight;
+    
+    public int getId() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getType() {
+        return this.type;
+    }
+    public int getWeight() {
+        return this.weight;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    
+}
---
+++ src/main/java/fr/ouadi/myApi/service/AnimalService.java
@@ -0,0 +1,10 @@
+package fr.ouadi.myApi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnimalService {
+    private Arraylist<Animal> animal;
+
+
+}
